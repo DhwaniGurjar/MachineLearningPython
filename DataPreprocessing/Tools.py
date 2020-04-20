f3bae88eb69beb42f70a2a49d9869c9addd97b9a
Dhwani <dhwanigurjar11@gmail.com>
2020-04-20 16:14:06 -0400

    -Encoding Categorical Data -Feature Scaling - Splitting Dataset

--- DataPreprocessing/Tools.py
+++ DataPreprocessing/Tools.py
@@ -16,3 +16,31 @@ imputer = SimpleImputer(missing_values = np.nan, strategy='mean')
 imputer.fit(x[:,1:3])
 x[:,1:3] = imputer.transform(x[:,1:3])
 print(x)
+
+#Encoding Categorical Data
+#Encoding the indepedent variable
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))          #X should be numpy array
+print(x)
+#Encoding the dependent variable
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+print(y)
+
+#Feature Scaling - Standardisation / Normalisation
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x = sc.fit_transform(x)
+print(x)
+
+#Splitting dataset into the Training Set(80% of data to train ML) and the Test Set(20% of data to evaluate prediction on new observations)
+from sklearn.model_selection import train_test_split    #tts is a function
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+print(x_train)
+print(x_test)
+print(y_train)
+print(y_test)
+ 
