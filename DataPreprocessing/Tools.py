37cb67173ca50c81e1d9cd5328517983101e3d44
Dhwani <dhwanigurjar11@gmail.com>
2020-04-20 14:56:24 -0400

    Taking care of missing data

--- DataPreprocessing/Tools.py
+++ DataPreprocessing/Tools.py
@@ -1,13 +1,18 @@
-#Importing Libraries
- 
+#Importing Libraries 
 import numpy as np                  #Math and Array
 import matplotlib.pyplot as plt     #Plot charts
 import pandas as pd                 #Import datasets
 
 #Importing Dataset
-
-dataset = pd.read_csv('Data.csv');      
+dataset = pd.read_csv('Data.csv')     
 x = dataset.iloc[:,:-1].values              #x = Matrix of Features - mostly in first column
 y = dataset.iloc[:,-1].values               #y = Dependent variables vector - mostly last column
 print(x)
 print(y)
+
+#Taking Care of Missing Data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy='mean')
+imputer.fit(x[:,1:3])
+x[:,1:3] = imputer.transform(x[:,1:3])
+print(x)
